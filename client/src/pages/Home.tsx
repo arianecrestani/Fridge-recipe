f8a0532a7c28e8d261f77a2ce03e315f0aa0e7b3
arianecrestani <ariane.mrv@gmail.com>
2023-05-17 09:40:50 +0200

    add the home and update the schema

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,44 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import ReactMarkdown from 'react-markdown'
+
 
 
 export const Home = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [input, setInput] = useState("");
+  const [markdown, setMarkdown] = useState ("")
+
+  const getApiData = async (e: FormEvent <HTMLFormElement>) => {
+    e.preventDefault()
+    const testUrl = `http://localhost:9000/api/recipes/recipe`;
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("ingredients", input);
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+    };
+
+   const response = await fetch(testUrl, requestOptions)
+   const result = await response.json()
+   console.log(result)
+   setMarkdown(result.choices[0].text)
+      
+  };
+
   return (
-    <div >Home</div>
-  )
-}
+    <div>
+      <form onSubmit={getApiData}>
+        enter the ingredients
+        <textarea onChange={(e) => setInput(e.target.value)}></textarea>
+        <button type="submit">submit</button>
+      </form>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+
+    </div>
+  );
+};
