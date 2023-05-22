77bfbd15f70f1141d1ef1238e0b7b5716709ed9c
arianecrestani <ariane.mrv@gmail.com>
2023-05-22 09:36:41 +0200

    style, and extract the tiltle of recipe to regex

--- client/src/pages/UserArea.tsx
+++ client/src/pages/UserArea.tsx
@@ -1,7 +1,7 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {}
-// Your user page component
+
 interface Favorite {
   _id: string;
   markdown: string;
@@ -9,10 +9,17 @@ interface Favorite {
   foodCategorie: string;
   __v: number;
 }
+
 export const UserArea = ({}: Props) => {
   const [favorites, setFavorites] = useState<Favorite[]>([]);
+  const [showDetails, setShowDetails] = useState<string | null>(null);
 
   console.log("loading component");
+
+  const toggleShowDetails = (favoriteId: string) => {
+    setShowDetails(showDetails === favoriteId ? null : favoriteId);
+  };
+
   const getApiData = async () => {
     const testUrl = `http://localhost:9000/api/users/favorites`;
 
@@ -22,9 +29,10 @@ export const UserArea = ({}: Props) => {
     console.log("token: ", tokenValue);
 
     myHeaders.append("Authorization", `Bearer ${tokenValue}`);
-
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
     console.log("fetching recipes");
+
     const requestOptions = {
       method: "GET",
       headers: myHeaders,
@@ -40,19 +48,44 @@ export const UserArea = ({}: Props) => {
     getApiData();
   }, []);
 
+  const extractFirstHeader = (markdown: string) => {
+    const match = markdown.match(/^#\s*(.+)$/m);
+    if (match) {
+      return match[1];
+    }
+    return null;
+  };
+
   return (
-    <div>
-    {favorites && (
-      <>
-        {favorites.map((favorite) => (
-          <div key={favorite._id}>
-            <p>Markdown: {favorite.markdown}</p>
-            <p>Author: {favorite.author}</p>
-            <p>Food Category: {favorite.foodCategorie}</p>
-          </div>
-        ))}
-      </>
-    )}
-  </div>
+    <div className="m-10">
+      {favorites && (
+        <>
+          {favorites.map((favorite) => (
+            <div
+              key={favorite._id}
+              className="max-w-md mx-auto bg-gray-200 p-4 mt-4 rounded"
+            >
+              <div
+                className="cursor-pointer flex items-center justify-between mb-2"
+                onClick={() => toggleShowDetails(favorite._id)}
+              >
+                <p className="text-xl font-bold text-orange-500">
+                  {extractFirstHeader(favorite.markdown)}
+                </p>
+                <span className="text-gray-600">
+                  {showDetails === favorite._id ? "-" : "+"}
+                </span>
+              </div>
+              {showDetails === favorite._id && (
+                <>
+                  <p>Markdown: {favorite.markdown}</p>
+                  <p>Food Category: {favorite.foodCategorie}</p>
+                </>
+              )}
+            </div>
+          ))}
+        </>
+      )}
+    </div>
   );
 };
