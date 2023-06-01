201d1fec0125d0406b48b442156807cf736f9749
arianecrestani <ariane.mrv@gmail.com>
2023-06-01 11:22:02 +0200

    fix style css

--- client/src/pages/UserArea.tsx
+++ client/src/pages/UserArea.tsx
@@ -11,7 +11,7 @@ export const UserArea = ({}: Props) => {
     <div className="m-24 flex justify-center">
       <div className="flex border-4 border-gray-400 p-8 m-8 rounded-lg">
         {user && (
-          <div className="flex gap-24">
+          <div className="flex">
             <div>
               <img
                 src={user.avatar}
@@ -21,15 +21,17 @@ export const UserArea = ({}: Props) => {
               <div className="ml-14">
                 <p className="text-xl m-8 font-bold">{user.username}</p>
                 <p className="text-gray-500">{user.email}</p>
-                <button className="p-4 m-4 bg-orange-500 text-white rounded-lg px-4">Update</button>
+                <button className="p-4 m-4 bg-orange-500 text-white rounded-lg px-4">
+                  Update
+                </button>
               </div>
-            </div>
-            <div className="flex">
-              <ShowFavorites />
             </div>
           </div>
         )}
       </div>
+      <div className="flex m-8">
+        <ShowFavorites />
+      </div>
     </div>
-  )
-}
+  );
+};
