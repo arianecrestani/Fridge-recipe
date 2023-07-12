b8d2724b1c9315eaa0d73b4ef542a3d2497d7f28
arianecrestani <ariane.mrv@gmail.com>
2023-07-12 15:25:18 +0200

    console.log to debug Node_env

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -41,11 +41,11 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
 
   console.log("active user : ", user);
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
   const [error] = useState<Error | null>(null);
 
   const login = async (email: string, password: string) => {
-
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     const urlencoded = new URLSearchParams();
@@ -77,7 +77,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
       }
     } catch (error) {
       console.log(error);
-      
+
       alert("Something went wrong - check console for error");
     }
   };
@@ -90,29 +90,32 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const checkForToken = () => {
     const token = localStorage.getItem("token");
     if (token) {
-      console.log("There is a token")
+      console.log("There is a token");
       fetchActiveUser(token);
     } else {
-      console.log("There is no token")
-      setUser(null)
+      console.log("There is no token");
+      setUser(null);
     }
-  }
-  const fetchActiveUser = async(token: string) => {
+  };
+  const fetchActiveUser = async (token: string) => {
     const myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`);
     const requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: myHeaders,
     };
     try {
-      const response = await fetch(`${process.env.REACT_APP_BASE_URL}users/active`, requestOptions);
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}users/active`,
+        requestOptions
+      );
       const result = await response.json();
       console.log("active user result:", result);
       setUser(result);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     checkForToken();
@@ -124,4 +127,3 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
     </AuthContext.Provider>
   );
 };
-
